940218735001deea2c7193f5630c86eb0d7e4458
bingland <bray.patriot@gmail.com>
2023-05-16 14:45:20 +0100

    Interface for ThemeContext, use react-jss

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import { createUseStyles } from 'react-jss';
+import { useState, createContext } from 'react'
+import { createUseStyles } from 'react-jss'
 
-const useStyles = createUseStyles({
-  thing: {
-    color: 'green',
-  },
-});
+import Landing from './Landing'
 
-const Hello = () => {
-  const css = useStyles();
-  return (
-    <div>
-      <div className={css.thing}>app</div>
-    </div>
-  );
-};
-
-function App() {
-  return (
-    <div className="App">
-      <Hello />
-    </div>
-  );
+interface ThemeContextInterface {
+  isDarkTheme: boolean;
+  setDarkTheme: (darkTheme: boolean) => void;
+  toggleTheme: () => void;
+  backgroundColor: string;
+  buttonFontColor: string;
+  fontColor: string;
+  borderColor: string;
+  themeColor: string;
 }
 
-export default App;
+export const ThemeContext = createContext<ThemeContextInterface>({
+  isDarkTheme: false,
+  setDarkTheme: () => {},
+  toggleTheme: () => {},
+  backgroundColor: '',
+  buttonFontColor: '',
+  fontColor: '',
+  borderColor: '',
+  themeColor: '',
+})
+
+function App() {
+
+  const [isDarkTheme, setDarkTheme] = useState(false)
+  const toggleTheme = () => setDarkTheme(!isDarkTheme)
+
+  const ThemeValues = {
+    isDarkTheme,
+    setDarkTheme,
+    toggleTheme,
+    backgroundColor: 'lightgreen',
+    buttonFontColor: '#ccc',
+    fontColor: 'grey',
+    borderColor: 'red',
+    themeColor: '#0070ce'
+  }
+
+  const { fontColor } = ThemeValues
+
+  const useStyles = createUseStyles({
+    appContainer: {
+      color: fontColor,
+    },
+  })
+  const css = useStyles()
+
+  return (
+    <ThemeContext.Provider value={ThemeValues}>
+      <div className={css.appContainer}>
+        <Landing />
+      </div>
+    </ThemeContext.Provider>
+  )
+}
+
+export default App
