24dbdd0cf8d4b36a50fdccf5ccca04e6f4a303aa
bingland <bray.patriot@gmail.com>
2023-06-22 02:11:46 -0600

    waitForElement and Chevron icons added

---
+++ src/utilities/index.ts
@@ -0,0 +1,20 @@
+export const waitForElement = (selector: any) => {
+  if (!selector) return null
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector))
+    }
+
+    const observer = new MutationObserver((mutations) => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector))
+        observer.disconnect()
+      }
+    })
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true
+    })
+  })
+}
