ed22f484a6da2d122438eae3da3ee9da7d0eead6
bingland <bray.patriot@gmail.com>
2023-06-25 05:48:03 -0600

    utilities; hexToRGBA function, useWindowDimesnions hook

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -18,3 +18,11 @@ export const waitForElement = (selector: any) => {
     })
   })
 }
+
+export const hexToRGBA = (hex: string, a:number = 1) => {
+  if (!hex) return 'rgba(0,0,0,0.5)'
+  const r = parseInt(hex.slice(1, 3), 16)
+  const g = parseInt(hex.slice(3, 5), 16)
+  const b = parseInt(hex.slice(5, 7), 16)
+  return `rgba(${r}, ${g}, ${b}, ${a})`
+}
